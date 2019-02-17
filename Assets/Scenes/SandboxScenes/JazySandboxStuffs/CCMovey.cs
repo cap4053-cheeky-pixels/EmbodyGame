681c1411e8b5fedbc20196e7062db8114b901820
Jazy Llerena <jazy@jazyserver.com>
2019-02-17 14:27:23 -0500

    Implement more tests for character movement/collisions
    NOTE Non of the code in my sandbox is meant to be used for the
    real game. In fact I would rather just remake them from scratch or
    possibly delete them later. Reason I am doing this is to share my
    testing environment for movement/collisions so that other team
    members can see the different implications of different
    combinations of RB/CC/Colliders/Triggers, since the Unity documentation
    is lacking when it comes to CC + collisions/triggers.

---
+++ Assets/Scenes/SandboxScenes/JazySandboxStuffs/CCMovey.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCMovey : MonoBehaviour
+{
+    public float offset = 5;
+    public float speed = 5;
+    private Vector3 initialPosition;
+    private CharacterController cc;
+    private Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        initialPosition = transform.position;
+        move = new Vector3(speed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((move.x > 0 && transform.position.x - initialPosition.x > offset)
+            || (move.x < 0 && transform.position.x - initialPosition.x < -offset))
+        {
+            move.x = -move.x;
+        }
+        cc.SimpleMove(move);
+    }
+}
---
+++ Assets/Scenes/SandboxScenes/JazySandboxStuffs/RBMovey.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBMovey : MonoBehaviour
+{
+    public float offset = 5;
+    public float force = 25;
+    public float speed = 3;
+    private Rigidbody rb;
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.isKinematic)
+        {
+            float moveX = initialPosition.x + Mathf.PingPong(Time.time * speed, offset*2) - offset;
+            transform.position = new Vector3(moveX, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            if ((force > 0 && transform.position.x - initialPosition.x > offset)
+                || (force < 0 && transform.position.x - initialPosition.x < -offset))
+            {
+                force = -force;
+            }
+
+            rb.AddForce(force, 0, 0);
+        }
+    }
+}
