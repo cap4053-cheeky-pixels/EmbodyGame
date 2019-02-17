e37f31c0e536499d3e8cc93249dd1b0546db4029
Jazy Llerena <jazy@jazyserver.com>
2019-02-16 20:09:02 -0500

    Create sandbox showing movement and collisions
    My sandbox shows some simple character movement and collision
    testing when the player has a rigidbody and is moved by forces.
    Some unexpected behaviors occur with collisions/triggers...
    Still not sure if this is better than the CharacterController

---
+++ Assets/Sandbox Scenes/JazySandboxStuffs/CCBasicMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCBasicMovement : MonoBehaviour
+{
+    public float speed;
+    private CharacterController playerCC;
+    void Start()
+    {
+        playerCC = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        //float inSprint = Input.GetAxis("Sprint");
+        float inSprint = Input.GetKey(KeyCode.LeftShift) ? 1 : 0;
+
+        Vector3 move = (transform.forward * vert) + (transform.right * hori);
+        move *= speed;
+
+        if (inSprint > 0)
+        {
+            // ?
+        }
+        else
+        {
+            playerCC.SimpleMove(move);
+        }
+    }
+}
---
+++ Assets/Sandbox Scenes/JazySandboxStuffs/CollisionTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("Trigger Enter: " + c.tag);
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        // Warning, spams the console
+        //Debug.Log("Trigger Stay: " + c.gameObject.tag);
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        Debug.Log("Trigger Exit: " + c.gameObject.tag);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("Collision Enter: " + c.collider.tag);
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        // Warning, spams the console
+        //Debug.Log("Collision Stay: " + c.collider.gameObject.tag);
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+        Debug.Log("Collision Exit: " + c.collider.gameObject.tag);
+    }
+
+}
