e5cc469a8916638287425b4fb69ddc36bf7002a2
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-17 20:19:38 -0500

    Add scripts to walls to dynamically resize them as needed using editor settings

---
+++ Assets/Scripts/Environmental/Room/WallWithoutDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallWithoutDoor : MonoBehaviour
+{
+    public float WallWidth = 30;
+
+
+    /* Called before the game starts. Sets up the wall with the correct dimensions.
+     */
+    void Awake()
+    {
+        GameObject wall = gameObject.transform.Find("Cube").gameObject;
+        Vector3 wallDimensions = wall.transform.localScale;
+        wall.transform.localScale = new Vector3(wallDimensions.x, wallDimensions.y, WallWidth);
+    }
+}
