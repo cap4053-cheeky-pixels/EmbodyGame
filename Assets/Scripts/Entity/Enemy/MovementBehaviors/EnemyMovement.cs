c9575ea849f583eafb7f854246daecd4c81b2b21
Jazy Llerena <jazy@jazyserver.com>
2019-02-20 19:16:03 -0500

    Create basic enemy prefab

---
+++ Assets/Scripts/Entity/Enemy/MovementBehaviors/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    This class should be on every enemy that can move. No path finding
+    or movement calculation should be done here, simple exposes a simple
+    move method for scripts that do path finding.
+*/
+[RequireComponent(typeof(CharacterController))]
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    private CharacterController cc;
+    private Vector3 cachedDirection;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        cc.detectCollisions = false;
+    }
+
+    // Method should only be called once per frame
+    public void Move(Vector3 direction)
+    {
+        cc.SimpleMove(direction * speed);
+    }
+}
---
+++ Assets/Scripts/Entity/Enemy/MovementBehaviors/WanderEnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class WanderEnemyMovement : MonoBehaviour
+{
+    private Vector3 currentDirection;
+    private EnemyMovement em;
+
+    void Awake()
+    {
+        em = GetComponent<EnemyMovement>();
+    }
+
+    void Start()
+    {
+        currentDirection = transform.forward;
+    }
+
+    void Update()
+    {
+        Wander();
+        em.Move(currentDirection);
+        transform.forward = currentDirection; // ew, get this shit outa here
+    }
+
+    void Wander()
+    {
+        // This is a very shit wander
+        currentDirection = Quaternion.Euler(0, Random.Range(-5, 5), 0) * currentDirection;
+    }
+}
