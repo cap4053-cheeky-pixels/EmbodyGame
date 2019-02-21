fbda74c903dbba583cf19cc658e3db3b1f73cf87
Jazy Llerena <jazy@jazyserver.com>
2019-02-20 19:16:32 -0500

    Update my sandbox with new stuffs

--- Assets/Scenes/SandboxScenes/JazySandboxStuffs/CollisionTest.cs
+++ Assets/Scenes/SandboxScenes/JazySandboxStuffs/CollisionTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollisionTest : MonoBehaviour
 {
     private float timer = 0;
+    private float delay = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,39 +20,46 @@ public class CollisionTest : MonoBehaviour
 
     void OnTriggerEnter(Collider c)
     {
+        if (timer < delay) return;
         Debug.Log(tag + " Trigger Enter: " + c.tag);
     }
 
     void OnTriggerStay(Collider c)
     {
+        if (timer < delay) return;
         // Warning, spams the console
         //Debug.Log(tag + " Trigger Stay: " + c.tag);
     }
 
     void OnTriggerExit(Collider c)
     {
+        if (timer < delay) return;
         Debug.Log(tag + " Trigger Exit: " + c.tag);
     }
 
     void OnCollisionEnter(Collision c)
     {
+        if (timer < delay) return;
         Debug.Log(tag + " Collision Enter: " + c.collider.tag);
     }
 
     void OnCollisionStay(Collision c)
     {
+        if (timer < delay) return;
         // Warning, spams the console
         //Debug.Log(tag + " Collision Stay: " + c.collider.tag);
     }
 
     void OnCollisionExit(Collision c)
     {
+        if (timer < delay) return;
         Debug.Log(tag + " Collision Exit: " + c.collider.tag);
     }
 
     // Unique to CharacterController, called like OnCollisionStay
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        if (timer < delay) return;
         // Constantly called when touching the floor
         if (hit.collider.tag == "Finish" || timer < 5) return;
         timer = 0;
