e4ce67bf73a5ca36b8f01026447597cf33853f90
klein1992 <ejw695@gmail.com>
2019-02-22 17:04:11 -0500

    Added actual Possession script not included in previous commit by error

---
+++ Assets/Scripts/Possession.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Possession : MonoBehaviour
+{
+
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.GetComponent<Player>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Dead" && Input.GetKeyDown("space")){
+            Enemy deadEnemy = other.gameObject.GetComponent<Enemy>();
+            player.MaxHealth = deadEnemy.MaxHealth;
+            player.Health = deadEnemy.MaxHealth;
+
+
+            // Possess the enemy model
+            GameObject playerModel = transform.Find("Model").gameObject;
+            GameObject enemyModel = deadEnemy.transform.Find("Model").gameObject;
+            // Clone the enemy model within the player
+            GameObject newModel = GameObject.Instantiate(enemyModel, transform);
+            newModel.name = "Model";
+
+            // Possess the enemy weapon
+            player.SetWeapon(deadEnemy.weapon);
+
+
+            // Cleanup
+            Destroy(playerModel);
+            // Remove the enemy
+            Destroy(other.gameObject);
+        }
+
+    }
+
+
+}
