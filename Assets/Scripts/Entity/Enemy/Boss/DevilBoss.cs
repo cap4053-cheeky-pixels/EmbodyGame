aa8c3e6e32e7cc3916ff37b878e324f2a1ea15dd
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-26 17:01:21 -0500

    Consolidate flying logic in boss script, separate from FlyBehavior. Add additional fields to the boss script.

--- Assets/Scripts/Entity/Enemy/Boss/DevilBoss.cs
+++ Assets/Scripts/Entity/Enemy/Boss/DevilBoss.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 
 
+[RequireComponent(typeof(Enemy))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class DevilBoss : MonoBehaviour
 {
     // The NavMeshAgent this boss will use to navigate the room
     private NavMeshAgent agent;
+
+    // Reference to the Enemy script attached to the boss
+    private Enemy self;
 
     // A reference to the player
     private GameObject player;
@@ -18,13 +24,18 @@ public class DevilBoss : MonoBehaviour
     // The distance within which the boss can initiate melee attacks
     public float meleeDistance;
 
+    // The animator for this boss
+    private Animator animator;
+    
 
     /* Initialize all members.
      */ 
     private void Awake()
     {
+        self = gameObject.GetComponent<Enemy>();
         agent = gameObject.GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
+        animator = self.model.GetComponent<Animator>();
 
         // When moving towards the player, the boss will always stop within its ranged attack radius
         agent.stoppingDistance = rangedDistance - 1;
@@ -39,4 +50,25 @@ public class DevilBoss : MonoBehaviour
     /* Returns a reference to the player this boss is fighting.
      */ 
     public GameObject GetPlayer() { return player; }
+
+
+    /* Plays the fly animation and moves the boss closer to the player.
+     * The boss will stop moving once the player is within its firing range.
+     */ 
+    public void FlyTowardsPlayer()
+    {
+        agent.SetDestination(player.transform.position);
+
+        // If we arrive at our destination, go back to Idle
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetTrigger("StopFlying");
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/Entity/Enemy/Boss/FlyBehavior.cs
+++ Assets/Scripts/Entity/Enemy/Boss/FlyBehavior.cs
@@ -6,34 +6,18 @@ using UnityEngine.AI;
 
 public class FlyBehavior : StateMachineBehaviour
 {
-    DevilBoss boss;
-    NavMeshAgent bossAgent;
+    private DevilBoss boss;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        // Yuck, I know
         boss = animator.gameObject.transform.parent.gameObject.GetComponent<DevilBoss>();
-        bossAgent = boss.GetAgent();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Fly towards the player
-        bossAgent.SetDestination(boss.GetPlayer().transform.position);
-
-        Debug.Log("Stopping: " + bossAgent.stoppingDistance + " Remaining: " + bossAgent.remainingDistance);
-
-        // If we arrive at our destination, go back to Idle
-        if (!bossAgent.pathPending)
-        {
-            if (bossAgent.remainingDistance <= bossAgent.stoppingDistance)
-            {
-                if (!bossAgent.hasPath || bossAgent.velocity.sqrMagnitude == 0f)
-                {
-                    animator.SetTrigger("StopFlying");
-                }
-            }
-        }
+        boss.FlyTowardsPlayer();
     }
 }
