b9c3b0a7be8d1a7923b6c72a4285f6355b63314c
Jazy Llerena <jazy@jazyserver.com>
2019-02-28 18:50:26 -0500

    Create AI attack controller for enemies

---
+++ Assets/Scripts/Entity/Enemy/AIAttackController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShootController))]
+public class AIAttackController : MonoBehaviour, IOnDeathController
+{
+    ShootController sc;
+    GameObject playerGO;
+
+    public void OnDeath()
+    {
+        enabled = false;
+    }
+
+    void Awake()
+    {
+        sc = GetComponent<ShootController>(); // Should be an AttackController interface
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Vector3 direction = playerGO.transform.position - transform.position;
+        direction.y = 0;
+        sc.FireWeaponTowards(direction.normalized);
+    }
+}
