018ac447e03c46cec0f49a5dfad2a303fef135e5
Jazy Llerena <jazy@jazyserver.com>
2019-03-01 18:56:33 -0500

    Create particle effect component for projectiles

---
+++ Assets/Scripts/Weapons/Projectile/ProjectileHitEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Projectile))]
+public class ProjectileHitEffect : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem hitParticleSystem;
+    private Projectile pro;
+
+    void Awake()
+    {
+        pro = GetComponent<Projectile>();
+    }
+
+    void Emit(Vector3 position, Vector3 direction)
+    {
+        if (hitParticleSystem == null) return;
+        Instantiate(hitParticleSystem, position + direction, Quaternion.LookRotation(direction));
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag != "Wall") return;
+
+        Vector3 position = c.ClosestPointOnBounds(transform.position);
+        Vector3 direction = Vector3.Normalize(-pro.velocity);
+        Emit(position, direction);
+    }
+}
