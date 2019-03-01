7bae45c281c0d59946cd6ed29a7cf0424490333f
Cameran Dodge <cameran.dodge@gmail.com>
2019-02-28 20:07:19 -0500

    Made expandable list of drops

--- Assets/Scripts/Entity/Enemy/EnemyDrop.cs
+++ Assets/Scripts/Entity/Enemy/EnemyDrop.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class EnemyDrop : MonoBehaviour
 {
+
+    public List<GameObject> heartPrefabs = new List<GameObject>();
+    public float healthDropProbability;
+
     /* Called when an enemy has died. Spawns a heart with random probability.
     */
-    public static void DropHeart(Transform t, float healthDropProbability, float heartTypeProbability)
+    public void DropHeart()
     {
-        var heartType = GameObject.Find("Half-Heart");
-        var heartTypeChance = Random.Range(0f, 1f);
-        if(heartTypeChance >= heartTypeProbability)
-        {
-            heartType = GameObject.Find("Heart");
-        }
+        var heartTypeChance = (int)Random.Range(0f, heartPrefabs.Count - 1);
+        var heartType = heartPrefabs[heartTypeChance];
 
-        var chance = Random.Range(0f, 1f);
+        var chance = Random.value;
         if (chance <= healthDropProbability)
         {
             var randX = Random.Range(-600, 600);
             var randZ = Random.Range(-600, 600);
-            var heart = Instantiate(heartType, t.position + new Vector3(0, 2.5f, 0), heartType.transform.rotation);
+            var heart = Instantiate(heartType, transform.position + new Vector3(0, 2.5f, 0), heartType.transform.rotation);
 
             var heartRb = heart.GetComponent<Rigidbody>();
 
