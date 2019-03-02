2c866c9cda17ea9b066037aea0152b6c54c1a10b
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-03-02 14:51:40 -0500

    Change spawning logic so it occurs only on an as-needed basis per room.

--- Assets/Scripts/Environmental/Room/RoomScript.cs
+++ Assets/Scripts/Environmental/Room/RoomScript.cs
@@ -13,18 +13,19 @@ public class RoomScript : MonoBehaviour
     private float desiredCameraHeight;
 
     private HashSet<GameObject> spawnedEnemies;
-    private SpawnScript spawner;
+    [SerializeField] GameObject spawnerObject;
+    private Spawner spawner;
     private int numEnemies;
 
+    private bool playerWasHereBefore;
 
-    /* Sets up the room by spawning enemies and subscribing to all their death events.
+
+    /* Sets up the room.
      */
     private void Awake()
     {
-        spawner = gameObject.transform.Find("SpawnPoints").gameObject.GetComponent<SpawnScript>();
-        spawnedEnemies = spawner.SpawnEnemies();
-        numEnemies = spawnedEnemies.Count;
-        SubscribeToEnemyDeath();
+        spawner = spawnerObject.GetComponent<Spawner>();
+        playerWasHereBefore = false;        
 
         cameraController = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
         desiredCameraHeight = 19.7f;
@@ -34,7 +35,7 @@ public class RoomScript : MonoBehaviour
     /* Allows this Room to listen to each enemy it has spawned in order to
      * detect enemy death. This allows it to control when the room doors open.
      */
-    void SubscribeToEnemyDeath()
+    private void SubscribeToEnemyDeath()
     {
         // Loop through each enemy
         foreach (GameObject enemyObject in spawnedEnemies)
@@ -51,30 +52,26 @@ public class RoomScript : MonoBehaviour
     /* Called each time an enemy dies in the current room. Used to update the
      * enemy count and to handle the case when all enemies have been slain.
      */
-    void OnEnemyDied(GameObject enemy)
+    private void OnEnemyDied(GameObject enemy)
     {
-        // enemy.GetComponent<Enemy>().SetMovement(false); // TODO re-enable once implemented
         spawnedEnemies.Remove(enemy);
         numEnemies--;
 
         // When all enemies in the room have been slain
         if (numEnemies == 0)
         {
-            UnlockAllDoors();
-
-            // Stop the theme music for the current room, if there is one
-            if(audioTheme != null && audioTheme.clip != null)
-            {
-                audioTheme.Stop();
-            }
+            OpenAllDoors();
+            StopThemeMusic();
+            
         }
     }
 
 
     /* Opens all doors for the current room.
      */
-    void UnlockAllDoors()
+    private void OpenAllDoors()
     {
+        // If we have doors, open them
         if(doors.Count != 0)
         {
             foreach (GameObject door in doors)
@@ -95,8 +92,9 @@ public class RoomScript : MonoBehaviour
 
     /* Closes all doors for the current room.
      */
-    void LockAllDoors()
+    private void CloseAllDoors()
     {
+        // If we have doors, lock them
         if(doors.Count != 0)
         {
             foreach (GameObject door in doors)
@@ -115,50 +113,73 @@ public class RoomScript : MonoBehaviour
     }
 
 
-    /* Loops through all live enemies in the room and dispatches them to go follow
-     * the player.
-     */
-    void DispatchEnemiesToFollowPlayer()
+    /* Plays the associated audio theme attached to the room object.
+     */  
+    private void PlayThemeMusic()
     {
-        foreach (GameObject enemyObject in spawnedEnemies)
+        if (audioTheme != null && audioTheme.clip != null && !audioTheme.isPlaying)
         {
-            Enemy enemy = enemyObject.gameObject.GetComponent<Enemy>();
+            audioTheme.Play();
+        }
+    }
 
-            if (enemy != null)
-            {
-                // The enemy's update loop condition will then be true
-                // enemy.SetMovement(true); // TODO re-enable once implemented
-            }
+
+    /* Stops the associated audio theme attached to the room object.
+     */ 
+    private void StopThemeMusic()
+    {
+        // Stop the theme music for the current room, if there is one
+        if (audioTheme != null && audioTheme.clip != null)
+        {
+            audioTheme.Stop();
         }
     }
 
 
     /* Called when any other collision object enters this Room. Used to detect when the player
-     * enters the room. If there are currently enemies, it will lock all doors.
+     * enters the room. If the player is entering the room for the first time and there are
+     * enemies to spawn, then the room spawns them. Also regulates door locking/unlocking.
      */
-    private void OnTriggerEnter(Collider c)
+    private void OnTriggerEnter(Collider other)
     {
-        // When the player enters this room
-        if (c.gameObject.tag == "Player")
+        // We only care about the player entering the room
+        if (other.gameObject.tag == "Player")
         {
-            // Move the camera to the new room
+            // Move the camera to this room's center
             cameraController.MoveTo(transform.position + new Vector3(0, desiredCameraHeight, 0));
 
-            // If there are enemies remaining, lock all doors and have enemies follow player
-            if (numEnemies != 0)
+            // If this is the first time the player is entering this room
+            if(!playerWasHereBefore)
             {
-                LockAllDoors();
-                DispatchEnemiesToFollowPlayer();
-
-                if (audioTheme != null && audioTheme.clip != null && !audioTheme.isPlaying)
+                // And there are enemies to spawn
+                if(spawner.Size() > 0)
                 {
-                    audioTheme.Play();
+                    CloseAllDoors();
+
+                    spawnedEnemies = spawner.SpawnEnemies();
+                    numEnemies = spawnedEnemies.Count;
+                    SubscribeToEnemyDeath();
+
+                    PlayThemeMusic();
                 }
+                // This will only be the case for rooms that have no enemies
+                else
+                {
+                    OpenAllDoors();
+                }
+
+                playerWasHereBefore = true;
             }
-            // If player entered an empty room, keep doors unlocked
+            // If instead the player has re-entered this room
             else
             {
-                UnlockAllDoors();
+                // Logically speaking, this seems a little paradoxical, but we need this check
+                // because the room's trigger collider is slightly smaller than the room itself
+                // Thus, it's possible that the player could "re-enter" a room from within the room
+                if(numEnemies == 0)
+                {
+                    OpenAllDoors();
+                }
             }
         }
     }
