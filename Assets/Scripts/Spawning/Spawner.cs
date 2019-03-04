fe46a06ce32e984f864965abfeaab458f43c1d43
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-03-04 14:03:12 -0500

    Add spawn delay mechanism. Separate audio theme from room and instead make it part of boss.

--- Assets/Scripts/Spawning/Spawner.cs
+++ Assets/Scripts/Spawning/Spawner.cs
@@ -8,6 +8,12 @@ public class Spawner : MonoBehaviour
 {
     // The points we'll poll for spawning
     private List<SpawnPoint> spawnPoints;
+    [SerializeField] private AudioSource spawnAudio;
+    [SerializeField] private float timeUntilSpawn;
+
+    // Used for signaling the completion of spawning routine
+    public delegate void Spawned(HashSet<GameObject> enemies);
+    public event Spawned spawningComplete;
 
 
     /* Sets up the spawner with all of its spawn points.
@@ -33,11 +39,28 @@ public class Spawner : MonoBehaviour
         return spawnPoints.Count;
     }
 
+    
+    /* Helper coroutine that initiates the entire spawning process.
+     */ 
+    private IEnumerator SpawnWithDelay()
+    {
+        // Delay spawning by this many seconds
+        yield return new WaitForSeconds(timeUntilSpawn);
 
-    /* Loops through all children SpawnPoint objects and spawns enemies at those points.
-     * Returns a HashSet of all spawned enemies for use by the rooms.
+        // Then, if we have something to spawn, play the audio and spawn
+        if(Size() > 0)
+        {
+            spawnAudio.Play();
+            Spawn();
+        }
+    }
+
+
+    /* Actually spawns the enemies in. Helper function used by SpawnEnemies.
+     * Loops through all children SpawnPoint objects and spawns enemies at those points.
+     * Signals the completion of spawning, passing along a hash set of all enemies.
      */
-    public HashSet<GameObject> SpawnEnemies()
+    private void Spawn()
     {
         HashSet<GameObject> spawned = new HashSet<GameObject>();
 
@@ -49,7 +72,8 @@ public class Spawner : MonoBehaviour
             // If an enemy is set to spawn at that point, go ahead and instantiate it
             if (enemyToSpawn != null)
             {
-                GameObject spawnedEnemy = Instantiate(enemyToSpawn, spawnPoint.transform.position, Quaternion.Euler(0, spawnPoint.yRotation, 0));
+                GameObject spawnedEnemy = Instantiate(enemyToSpawn, spawnPoint.transform.position, 
+                                                      Quaternion.Euler(0, spawnPoint.yRotation, 0));
                 Enemy script = spawnedEnemy.GetComponent<Enemy>();
 
                 script.MaxHealth = spawnPoint.maxHealth;
@@ -60,6 +84,26 @@ public class Spawner : MonoBehaviour
             }
         }
 
-        return spawned;
+        spawningComplete?.Invoke(spawned);
+        HideSpawnPoints();
+    }
+
+
+    /* Disables each spawn point so it no longer appears in game.
+     */ 
+    private void HideSpawnPoints()
+    {
+        foreach(SpawnPoint spawnPoint in spawnPoints)
+        {
+            spawnPoint.gameObject.SetActive(false);
+        }
+    }
+
+
+    /* Public-facing function to be called by RoomScript.cs.
+     */
+    public void SpawnEnemies()
+    {
+        StartCoroutine(SpawnWithDelay());
     }
 }
