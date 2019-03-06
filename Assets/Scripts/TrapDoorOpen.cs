2187d870196ae257ae7f696dbb10ccdbd9a97040
klein1992 <ejw695@gmail.com>
2019-03-06 00:28:36 -0500

    Added event and event listener to TrapDoor

--- Assets/Scripts/TrapDoorOpen.cs
+++ Assets/Scripts/TrapDoorOpen.cs
@@ -7,15 +7,20 @@ public class TrapDoorOpen : MonoBehaviour
     [SerializeField]
     private GameObject TrapDoor;
     
+    public delegate void TrapOpenedHandler();
+    public event TrapOpenedHandler TrapOpened;
+    
     void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == "Player"){
             //Play animations
             TrapDoor.transform.GetChild(0).GetComponent<Animation>().Play("LeftAnim");
             TrapDoor.transform.GetChild(1).GetComponent<Animation>().Play("HingeRight");
-            //Start script that allows player to fall
-            TrapDoor.GetComponent<PlayerFall>().trapOpened = true;
+            
+            //signal event
+            TrapOpened?.Invoke();
         }
         else
         Debug.Log("Entity detected" + other.gameObject.tag);
     }
+    
 }
