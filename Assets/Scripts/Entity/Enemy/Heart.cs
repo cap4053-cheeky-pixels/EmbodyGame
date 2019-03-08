985d0a1c4b33331a5aee9cc6ae0b8af02cb56b78
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-03-07 19:47:49 -0500

    Delay destruction of heart to give time for audio to play. Hide heart in the meantime.

--- Assets/Scripts/Entity/Enemy/Heart.cs
+++ Assets/Scripts/Entity/Enemy/Heart.cs
@@ -6,40 +6,65 @@ public class Heart : MonoBehaviour
 {
     public int Health;
     public bool Golden;
+    [SerializeField] private AudioSource heartPickupAudio;
+    private float timeUntilHeartDestroyed = 0.5f;
+
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Player player = other.gameObject.GetComponent<Player>();
+
             if (Golden)
             {
                 if((player.GoldenHealth + Health) <= player.MaxGoldenHealth)
                 {
                     player.ChangeHealthBy(player.MaxHealth - player.Health);
                     player.ChangeGoldenHealthBy(Health);
-                    Destroy(transform.gameObject);
+                    StartCoroutine(DestroyWithDelay());
                 }
                 else if (player.GoldenHealth != player.MaxGoldenHealth)
                 {
                     player.ChangeHealthBy(player.MaxHealth - player.Health);
                     player.ChangeGoldenHealthBy(player.MaxGoldenHealth - player.Health);
-                    Destroy(transform.gameObject);
+                    StartCoroutine(DestroyWithDelay());
                 }
             }
-            else
+            else if(player.Health != player.MaxHealth)
             {
                 if ((player.Health + Health) <= player.MaxHealth)
                 {
                     player.ChangeHealthBy(Health);
-                    Destroy(transform.gameObject);
+                    StartCoroutine(DestroyWithDelay());
                 }
                 else if (player.Health != player.MaxHealth)
                 {
                     player.ChangeHealthBy(player.MaxHealth - player.Health);
-                    Destroy(transform.gameObject);
+                    StartCoroutine(DestroyWithDelay());
                 }
             }
         }
+    }
+
+
+    private void HideHeart()
+    {
+        BoxCollider collider = gameObject.GetComponent<BoxCollider>();
+        Rigidbody body = gameObject.GetComponent<Rigidbody>();
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        collider.enabled = false;
+        body.mass = 0.0f;
+        renderer.enabled = false;
+    }
+
+
+    private IEnumerator DestroyWithDelay()
+    {
+        HideHeart();
+        heartPickupAudio.Play();
+        yield return new WaitForSeconds(heartPickupAudio.clip.length);
+        Destroy(gameObject);
     }
 }
