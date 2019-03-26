1133053b6b3fe485e992cf0e01e94acbca4aacf4
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-03-26 15:08:51 -0400

    Fix WanderMovement bug

--- Assets/Scripts/Entity/Enemy/MovementBehaviors/WanderMovement.cs
+++ Assets/Scripts/Entity/Enemy/MovementBehaviors/WanderMovement.cs
@@ -27,37 +27,55 @@ public class WanderMovement : MonoBehaviour, IOnDeathController
     {
         NavMeshPath path;
 
-        do
+        // Run this code only as long as the wander behavior is enabled
+        while(enabled)
         {
             path = new NavMeshPath();
 
+            // Pick a random direction for the agent to head in
             Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
             randomDirection += transform.position;
 
-            NavMeshHit hit;
-            NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, 1 << NavMesh.GetAreaFromName("Walkable"));
+            // Sample points on the navmesh until we get a hit (this usually terminates in one iteration except for an edge case)
+            NavMeshHit navMeshHit;
+            do
+            {
+                int areaMask = 1 << NavMesh.GetAreaFromName("Walkable");
+                NavMesh.SamplePosition(randomDirection, out navMeshHit, wanderRadius, areaMask);
 
-            Vector3 destination = hit.position;
+                if(!navMeshHit.hit) Debug.Log(navMeshHit.hit);
+            }
+            while (!navMeshHit.hit);
+
+            // Calculate a path to the destination we generated above
+            Vector3 destination = navMeshHit.position;
             movement.agent.CalculatePath(destination, path);
 
-            // This check is necessary to ensure that the agent only visits valid locations
-            if(path.status != NavMeshPathStatus.PathInvalid)
+            // Ensure that the agent only visits valid locations
+            if (path.status != NavMeshPathStatus.PathInvalid)
             {
-                // TODO: there's a random edge case where the position is still invalid: "CalculatePolygonPath: invalid target position { Infinity, Infinity, Infinity }"
+                // Move the agent there
                 movement.MoveTo(destination);
 
                 // Some loss of time due to these computations, but negligible
                 float distanceToDestination = Vector3.Distance(transform.position, destination);
-                float timeToWaitBeforeRecalculation = distanceToDestination / movement.agent.speed;
+                float timeToWaitBeforeRecalculation = 0.0f;
 
+                if (movement.agent.speed != 0.0f)
+                {
+                    timeToWaitBeforeRecalculation = distanceToDestination / movement.agent.speed;
+                }
+
+                // Wait for roughly however many seconds it would've taken to get to the destination
                 yield return new WaitForSeconds(timeToWaitBeforeRecalculation);
             }
+
+            // If a valid path was not generated, just resume on the next frame update
             else
             {
                 yield return null;
             }
         }
-        while (enabled);
     }
 
 
