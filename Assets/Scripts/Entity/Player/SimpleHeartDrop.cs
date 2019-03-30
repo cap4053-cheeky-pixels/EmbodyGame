9a9af0f55cde4cb7a836adc564c58bbab4bbdaea
klein1992 <ejw695@gmail.com>
2019-03-29 23:36:48 -0400

    Edited DropHeart method toallow for half heart drops

--- Assets/Scripts/Entity/Player/SimpleHeartDrop.cs
+++ Assets/Scripts/Entity/Player/SimpleHeartDrop.cs
@@ -6,14 +6,20 @@ public class SimpleHeartDrop : MonoBehaviour
 {
     public float spawnHeight = 2.5f;
     public int xzRange = 600;
-    public int yForce = 800;
+    public int yForce = 400;
     [SerializeField]
-    GameObject Heartprefab;
+    GameObject FullHeartprefab;
+    [SerializeField]
+    GameObject HalfHeartprefab;
+    GameObject HeartType;
     
-    public void DropHeart()
+    public void DropHeart(string whichprefab)
     {
-        GameObject heart = Instantiate(Heartprefab, transform.position + new Vector3(0, spawnHeight, 0), Heartprefab.transform.rotation);
-        //Rigidbody heartRb = heart.GetComponent<Rigidbody>();
-        //heartRb.AddForce(new Vector3(Random.Range(-xzRange, xzRange), yForce, Random.Range(-xzRange, xzRange)));
+        HeartType = FullHeartprefab;
+        if(whichprefab != "Full")
+        HeartType = HalfHeartprefab;
+        GameObject heart = Instantiate(HeartType, transform.position + new Vector3(0, spawnHeight, 0), FullHeartprefab.transform.rotation);
+        Rigidbody heartRb = heart.GetComponent<Rigidbody>();
+        heartRb.AddForce(new Vector3(Random.Range(-xzRange, xzRange), yForce, Random.Range(-xzRange, xzRange)));
     }
 }
