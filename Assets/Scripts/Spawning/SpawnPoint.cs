87368b5fe529161c7476eff789b395eb8c14f4f7
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-04-05 13:15:00 -0400

    Randomize spawns

--- Assets/Scripts/Spawning/SpawnPoint.cs
+++ Assets/Scripts/Spawning/SpawnPoint.cs
@@ -10,13 +10,13 @@ public class SpawnPoint : MonoBehaviour
     public float scale = 1.0f;
     public float yRotation = 0;
 
-
     /**
      * Returns a random enemy to spawn from the internal pool of candidates.
-     */ 
+     */
     public GameObject GetRandomEnemyToSpawn()
     {
-        int index = Random.Range(0, enemyPrefabs.Count - 1);
+        Random.InitState(System.DateTime.Now.Millisecond);
+        int index = Random.Range(0, enemyPrefabs.Count);
 
         return enemyPrefabs[index];
     }
